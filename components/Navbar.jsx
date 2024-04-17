0f12e291a1f2f5a9784ac17595356ba6aef83d19
Milaadmg023 <132762516+Milaadmg023@users.noreply.github.com>
2024-04-17 08:20:24 +0330

    add : creating mobile nav

---
+++ components/Navbar.jsx
@@ -0,0 +1,93 @@
+import Image from "next/image";
+import Link from "next/link";
+const homeIcon = () => {
+  return (
+    <svg
+      fill="none"
+      height="32"
+      viewBox="0 0 24 24"
+      width="32"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M9 22V12H15V22M3 9L12 2L21 9V20C21 20.5304 20.7893 21.0391 20.4142 21.4142C20.0391 21.7893 19.5304 22 19 22H5C4.46957 22 3.96086 21.7893 3.58579 21.4142C3.21071 21.0391 3 20.5304 3 20V9Z"
+        stroke="currentColor"
+      />
+    </svg>
+  );
+};
+
+const infoIcon = () => {
+  return (
+    <svg
+      className="bi bi-info-circle"
+      fill="currentColor"
+      height="16"
+      viewBox="0 0 16 16"
+      width="16"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+      <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0" />
+    </svg>
+  );
+};
+
+const contactIcon = () => {
+  return (
+    <svg
+      className="bi bi-telephone"
+      fill="currentColor"
+      height="16"
+      viewBox="0 0 16 16"
+      width="16"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z" />
+    </svg>
+  );
+};
+
+const Navbar = () => {
+  const menuItems = [
+    {
+      id: 1,
+      name: "درباره ما",
+      link: "/about",
+      icon: infoIcon(),
+    },
+    {
+      id: 2,
+      name: "خانه",
+      link: "/",
+      icon: homeIcon(),
+    },
+    {
+      id: 3,
+      name: "ارتباط با ما",
+      link: "/contact",
+      icon: contactIcon(),
+    },
+  ];
+  return (
+    <>
+      {/* Mobile Menu */}
+      <div className="flex flex-col fixed bottom-0 left-0 right-0 bg-white w-screen mx-auto">
+        <div className="border border-gray-300 py-3 flex justify-between gap-1 shadow-xl rounded-md">
+          {menuItems.map((item) => (
+            <div key={item.id} className="group relative px-4 cursor-pointer">
+              <div className="flex h-10 w-10 items-center justify-center rounded-full hover:text-blue-500">
+                <Link href={item.link}>{item.icon}</Link>
+              </div>
+              <span className="absolute -top-8 left-[50%] -translate-x-[50%] z-20 origin-left scale-0 rounded-lg border border-gray-300 bg-white px-3 py-2 text-sm font-medium shadow-md transition-all duration-300 ease-in-out group-hover:scale-100">
+                {item.name}
+              </span>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
